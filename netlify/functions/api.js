cb34ed1fb013d9b04b9cdcd586c257c9f7ad419d
LustyFlix <111301802+LustyFlix@users.noreply.github.com>
2026-01-29 23:46:17 +0600

    Implement Netlify

---
+++ netlify/functions/api.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+app.use(express.json());
+
+const apiRoutes = require('../../src/routes/api');
+
+app.use('/api', apiRoutes);
+
+app.get('/docs', (req, res) => {
+  res.json(require('../../public/docs/docs.json'));
+});
+
+app.get('/docs/genre', (req, res) => {
+  res.json(require('../../public/docs/genre.json'));
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 'error',
+    message: 'Route not found. Check our docs. /docs'
+  });
+});
+
+module.exports.handler = serverless(app);
