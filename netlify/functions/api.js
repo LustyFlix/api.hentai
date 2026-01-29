57ee7063ea9718ded8ca47798cc8ebd49fc77e5b
LustyFlix <111301802+LustyFlix@users.noreply.github.com>
2026-01-30 00:27:07 +0600

    Refactor API routes and add documentation endpoints

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,21 +5,32 @@ const serverless = require('serverless-http');
 const app = express();
 app.use(express.json());
 
-/* ✅ TEST ROUTE — MUST BE FIRST */
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', source: 'netlify-function' });
-});
 
-/* routes */
 const apiRoutes = require('../../src/routes/api');
+
 app.use('/', apiRoutes);
 
-/* ❌ 404 — MUST BE LAST */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+app.get('/docs', (req, res) => {
+    const docs = require('../../public/docs/docs.json');
+    res.json(docs);
+});
+
+app.get('/docs/genre', (req, res) => {
+    const docs = require('../../public/docs/genre.json');
+    res.json(docs);
+});
+
+
+
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    message: 'Route not found. Check our docs. /docs'
-  });
+    res.status(404).json({
+        status: 'error',
+        message: 'Route not found. Check our docs. /docs'
+    });
 });
 
 module.exports.handler = serverless(app);
