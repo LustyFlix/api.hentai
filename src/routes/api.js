8c4abc9c8655bc71052c264f82b19b0e15bd1f39
LustyFlix <111301802+LustyFlix@users.noreply.github.com>
2026-01-30 00:17:19 +0600

    Update API routes to include '/api' prefix

--- src/routes/api.js
+++ src/routes/api.js
@@ -15,23 +15,23 @@ const handleResponse = (res, promise) => {
 };
 
 // HentaiTV endpoints
-router.get('/hen/tv/watch/:id', (req, res) => handleResponse(res, hentaitv.scrapeWatch(req.params.id)));
-router.get('/hen/tv/info/:id', (req, res) => handleResponse(res, hentaitv.scrapeInfo(req.params.id)));
-router.get('/hen/tv/search/:query/:page?', (req, res) => handleResponse(res, hentaitv.scrapeSearch(req.params.query, req.params.page || 1)));
-router.get('/hen/tv/genre/:genre/:page?', (req, res) => handleResponse(res, hentaitv.scrapeGenre(req.params.genre, req.params.page || 1)));
-router.get('/hen/tv/recent', (req, res) => handleResponse(res, hentaitv.scrapeRecent()));
-router.get('/hen/tv/trending', (req, res) => handleResponse(res, hentaitv.scrapeTrending()));
-router.get('/hen/tv/random', (req, res) => handleResponse(res, hentaitv.scrapeRandom()));
-router.get('/hen/tv/brand/:brand', (req, res) =>
+router.get('/api/hen/tv/watch/:id', (req, res) => handleResponse(res, hentaitv.scrapeWatch(req.params.id)));
+router.get('/api/hen/tv/info/:id', (req, res) => handleResponse(res, hentaitv.scrapeInfo(req.params.id)));
+router.get('/api/hen/tv/search/:query/:page?', (req, res) => handleResponse(res, hentaitv.scrapeSearch(req.params.query, req.params.page || 1)));
+router.get('/api/hen/tv/genre/:genre/:page?', (req, res) => handleResponse(res, hentaitv.scrapeGenre(req.params.genre, req.params.page || 1)));
+router.get('/api/hen/tv/recent', (req, res) => handleResponse(res, hentaitv.scrapeRecent()));
+router.get('/api/hen/tv/trending', (req, res) => handleResponse(res, hentaitv.scrapeTrending()));
+router.get('/api/hen/tv/random', (req, res) => handleResponse(res, hentaitv.scrapeRandom()));
+router.get('/api/hen/tv/brand/:brand', (req, res) =>
     handleResponse(res, hentaitv.scrapeBrand(req.params.brand, req.query.page || 1))
 );
 
 // HentaiCity endpoints
-router.get('/hen/city/info/:id', (req, res) => handleResponse(res, hentaicity.scrapeInfo(req.params.id)));
-router.get('/hen/city/watch/:id', (req, res) => handleResponse(res, hentaicity.scrapeWatch(req.params.id)));
-router.get('/hen/city/recent', (req, res) => handleResponse(res, hentaicity.scrapeRecent()));
-router.get('/hen/city/popular/:page?', (req, res) => {const page = req.params.page ? parseInt(req.params.page, 10) : 1; handleResponse(res, hentaicity.scrapePopular(page));});
-router.get('/hen/city/top/:page?', (req, res) => {const page = req.params.page ? parseInt(req.params.page, 10) : 1; handleResponse(res, hentaicity.scrapeTop(page));});
+router.get('/api/hen/city/info/:id', (req, res) => handleResponse(res, hentaicity.scrapeInfo(req.params.id)));
+router.get('/api/hen/city/watch/:id', (req, res) => handleResponse(res, hentaicity.scrapeWatch(req.params.id)));
+router.get('/api/hen/city/recent', (req, res) => handleResponse(res, hentaicity.scrapeRecent()));
+router.get('/api/hen/city/popular/:page?', (req, res) => {const page = req.params.page ? parseInt(req.params.page, 10) : 1; handleResponse(res, hentaicity.scrapePopular(page));});
+router.get('/api/hen/city/top/:page?', (req, res) => {const page = req.params.page ? parseInt(req.params.page, 10) : 1; handleResponse(res, hentaicity.scrapeTop(page));});
 
 // MangaKakalot endpoints
 router.get("/manga/kakalot/read/:mangaId?/:chapterId?", mangakakalot.getMangaChapterImages);
@@ -45,16 +45,17 @@ router.get("/manga/kakalot/popular-now", mangakakalot.getPopularNowMangas);
 router.get("/manga/kakalot/home", mangakakalot.getHomePage);
 
 // JavGG endpoints
-router.get('/jav/javgg/recent/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggRecent(req.params.page || 1)));
-router.get('/jav/javgg/featured/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggFeatured(req.params.page || 1)));
-router.get('/jav/javgg/trending/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggTrending(req.params.page || 1)));
-router.get('/jav/javgg/random/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggRandom(req.params.page || 1)));
-router.get('/jav/javgg/search/:query/:page?', (req, res) => handleResponse(res, javgg.scrapeSearch(req.params.query, req.params.page || 1)));
-router.get('/jav/javgg/info/:id', (req, res) => handleResponse(res, javgg.scrapeJavDetails(req.params.id)));
-router.get('/jav/javgg/servers/:id', (req, res) => handleResponse(res, javgg.scrapeJavServers(req.params.id)));
-router.get('/jav/javgg/watch/:id', (req, res) => handleResponse(res, javggvidlink.scrapeJavVid(req.params.id, req.query.server)));
-router.get('/jav/javgg/watch/:id/:server', (req, res) => handleResponse(res, javggvidlink.scrapeJavVid(req.params.id, req.params.server)));
-router.get('/jav/javgg/genre/:genre/:page?', (req, res) => handleResponse(res, javgg.scrapeJavGenre(req.params.genre, req.params.page || 1)));
+router.get('/api/jav/javgg/recent/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggRecent(req.params.page || 1)));
+router.get('/api/jav/javgg/featured/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggFeatured(req.params.page || 1)));
+router.get('/api/jav/javgg/trending/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggTrending(req.params.page || 1)));
+router.get('/api/jav/javgg/random/:page?', (req, res) => handleResponse(res, javgg.scrapeJavggRandom(req.params.page || 1)));
+router.get('/api/jav/javgg/search/:query/:page?', (req, res) => handleResponse(res, javgg.scrapeSearch(req.params.query, req.params.page || 1)));
+router.get('/api/jav/javgg/info/:id', (req, res) => handleResponse(res, javgg.scrapeJavDetails(req.params.id)));
+router.get('/api/jav/javgg/servers/:id', (req, res) => handleResponse(res, javgg.scrapeJavServers(req.params.id)));
+router.get('/api/jav/javgg/watch/:id', (req, res) => handleResponse(res, javggvidlink.scrapeJavVid(req.params.id, req.query.server)));
+router.get('/api/jav/javgg/watch/:id/:server', (req, res) => handleResponse(res, javggvidlink.scrapeJavVid(req.params.id, req.params.server)));
+router.get('/api/jav/javgg/genre/:genre/:page?', (req, res) => handleResponse(res, javgg.scrapeJavGenre(req.params.genre, req.params.page || 1)));
+
 
 
 
